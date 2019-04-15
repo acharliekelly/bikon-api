ded84950d99b0fffe4d965f43dcacdd9755e7f16
Charlie <charliekelly31415@gmail.com>
2019-04-15 16:17:33 -0400

    Custom route for own reports

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :examples, except: %i[new edit]
   resources :condreps, except: %i[new edit]
 
+  get '/my-condreps' => 'condreps#index_own'
+
   # Custom routes
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
