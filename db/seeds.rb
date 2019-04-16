d205b5d839907ecce3d74771117eae3c793a9950
Charlie <charliekelly31415@gmail.com>
2019-04-16 15:57:24 -0400

    Create seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,8 +9,83 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-condreps = Condrep.create([
-  { condition: 1, geolat: 42.3530667, geolong: -71.0596961,
-    reported_at: Date.now(), user_id: 1 }
+Condrep.create([
+  # {
+  #   condition: 1,
+  #   geolat: 42.3530667,
+  #   geolong: -71.0596961,
+  #   user_id: 1
+  # },
+  {
+    condition: 1,
+    geolat: 42.3683005,
+    geolong: -71.2218484,
+    user_id: 5
+  },
+  {
+    condition: 2,
+    geolat: 42.3655023,
+    geolong: -71.2153682,
+    user_id: 5
+  },
+  {
+    condition: 1,
+    geolat: 42.3647969,
+    geolong: -71.2148957,
+    user_id: 5
+  },
+  {
+    condition: 1,
+    geolat: 42.3638449,
+    geolong: -71.2098427,
+    user_id: 5
+  },
+  {
+    condition: 2,
+    geolat: 42.3659849,
+    geolong: -71.2056047,
+    user_id: 5
+  },
+  {
+    condition: 1,
+    geolat: 42.3674429,
+    geolong: -71.2011847,
+    user_id: 5
+  },
+  {
+    condition: 3,
+    geolat: 42.3667059,
+    geolong: -71.1991997,
+    user_id: 5
+  },
+  {
+    condition: 2,
+    geolat: 42.3657549,
+    geolong: -71.1942537,
+    user_id: 5
+  },
+  {
+    condition: 1,
+    geolat: 42.3646529,
+    geolong: -71.1941037,
+    user_id: 5
+  },
+  {
+    condition: 4,
+    geolat: 42.3642578,
+    geolong: -71.1879933,
+    user_id: 5
+  },
+  {
+    condition: 1,
+    geolat: 42.3592149,
+    geolong: -71.1604457,
+    user_id: 5
+  },
+  {
+    condition: 1,
+    geolat: 42.3643119,
+    geolong: -71.1449747,
+    user_id: 5
+  }
 ])
