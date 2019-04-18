988900b858fd9a048fa265d03b10ac342a27e5ee
Charlie <charliekelly31415@gmail.com>
2019-04-18 16:38:19 -0400

    Update seeds
    only 2 users on production db

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,72 +20,72 @@ Condrep.create([
     condition: 1,
     geolat: 42.3683005,
     geolong: -71.2218484,
-    user_id: 5
+    user_id: 2
   },
   {
     condition: 2,
     geolat: 42.3655023,
     geolong: -71.2153682,
-    user_id: 5
+    user_id: 1
   },
   {
     condition: 1,
     geolat: 42.3647969,
     geolong: -71.2148957,
-    user_id: 5
+    user_id: 1
   },
   {
     condition: 1,
     geolat: 42.3638449,
     geolong: -71.2098427,
-    user_id: 5
+    user_id: 2
   },
   {
     condition: 2,
     geolat: 42.3659849,
     geolong: -71.2056047,
-    user_id: 5
+    user_id: 1
   },
   {
     condition: 1,
     geolat: 42.3674429,
     geolong: -71.2011847,
-    user_id: 5
+    user_id: 1
   },
   {
     condition: 3,
     geolat: 42.3667059,
     geolong: -71.1991997,
-    user_id: 5
+    user_id: 2
   },
   {
-    condition: 2,
+    condition: 5,
     geolat: 42.3657549,
     geolong: -71.1942537,
-    user_id: 5
+    user_id: 2
   },
   {
     condition: 1,
     geolat: 42.3646529,
     geolong: -71.1941037,
-    user_id: 5
+    user_id: 2
   },
   {
     condition: 4,
     geolat: 42.3642578,
     geolong: -71.1879933,
-    user_id: 5
+    user_id: 1
   },
   {
-    condition: 1,
+    condition: 4,
     geolat: 42.3592149,
     geolong: -71.1604457,
-    user_id: 5
+    user_id: 1
   },
   {
-    condition: 1,
+    condition: 5,
     geolat: 42.3643119,
     geolong: -71.1449747,
-    user_id: 5
+    user_id: 2
   }
 ])
