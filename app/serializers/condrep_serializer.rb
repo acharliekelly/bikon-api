33a604c83f9c225f1ca2236421deaaedfbd7cb8a
Charlie <charliekelly31415@gmail.com>
2019-04-18 13:41:56 -0400

    format response object

--- app/serializers/condrep_serializer.rb
+++ app/serializers/condrep_serializer.rb
@@ -1,8 +1,19 @@
 class CondrepSerializer < ActiveModel::Serializer
-  attributes :id, :condition, :geolat, :geolong, :occurred,
-             :notes, :user_id, :editable
+  attributes :id, :condition, :geolat, :geolong, :occurred, :when, :secondsago,
+             :notes, :user_id, :email, :editable
+  def email
+    object.user.email
+  end
 
   def editable
     scope == object.user
+  end
+
+  def when
+    object.occurred.strftime('%m/%d/%Y at %I:%m %p')
+  end
+
+  def secondsago
+    (Time.now - object.occurred).to_i
   end
 end
