0894c27e8afcf20da61bacd3953954fae4b7a78b
Charlie <charliekelly31415@gmail.com>
2019-04-22 06:48:20 -0400

    set default condition to 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_16_194925) do
+ActiveRecord::Schema.define(version: 2019_04_21_193412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "condreps", force: :cascade do |t|
-    t.integer "condition"
+    t.integer "condition", default: 0
     t.decimal "geolat"
     t.decimal "geolong"
     t.datetime "created_at", null: false
